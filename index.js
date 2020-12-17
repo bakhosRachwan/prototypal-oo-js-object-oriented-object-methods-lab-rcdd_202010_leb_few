900c67e70269b6c2337fb1da6a84c4e927a8d38f
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-17 08:11:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 function BoardMember (name, homeState, training){
+=======
+function BroadMember (name, homeState, training){
+>>>>>>> d51e7a70a210f4de193e41824703d52824d3bb48
   this.name = name;
   this.homeState = homeState;
   this.training =  training;
 }
+<<<<<<< HEAD
  BoardMember.prototype.veto = () => {
   return `No, I must disagree`;
 }
@@ -17,4 +22,20 @@ BoardMember.prototype.releasePressStatement = () => {
 }
 BoardMember.prototype.sayHi = function(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+=======
+ veto instanceof BroadMember{
+  return `No, I must disagree`;
+}
+function approve (){
+  return `You can do that!`;
+}
+function doCharity (){
+  return `I loke to help people.`;
+}
+function releasePressStatement  (){
+  return `You will see great things from Scuber.`;
+}
+function sayHi (){
+  return `"Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`;
+>>>>>>> d51e7a70a210f4de193e41824703d52824d3bb48
 }
